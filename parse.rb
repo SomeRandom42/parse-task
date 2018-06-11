3feed1865fd1c908766d6c4cdee1726c79955b46
Patrick Lewis <PatrickDanielLewis@gmail.com>
2018-06-11 18:54:12 +0000

    Minor tweaks and testing pulling data into an array from differrent files

--- parse.rb
+++ parse.rb
@@ -1,3 +1,24 @@
+array = []
+
 File.open("space.txt").each do |line|
     puts line
+    array.push(line)
 end    
+
+File.open("comma.txt").each do |line|
+    puts line
+    array.push(line)
+end    
+
+
+File.open("pipe.txt").each do |line|
+    puts line
+    array.push(line)
+end    
+
+puts "\n"
+puts "Array storing all the data:"
+puts array
+puts "\n"
+puts "Puts Array Point 1:"
+puts array[0]
